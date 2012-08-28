2860f40de4c139e22c65603a6630c1f88fc6eaf3
Nathan Oehlman <nathan.oehlman@sidelab.com>
2012-08-28 11:44:17 +1000

    Check for the send method first

--- client.js
+++ client.js
@@ -4,5 +4,7 @@ var _ = require('underscore');
   Instructs Steelmesh that this client is ready for requests
  **/
 exports.ready = function(port, data) {
-    process.send(_.extend(data || {}, {operation: 'steelmesh.client.up', port: port}));
+    if (process.send) {
+        process.send(_.extend(data || {}, {operation: 'steelmesh.client.up', port: port}));
+    }
 }
